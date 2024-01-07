8cb342226aef4eff43d3e3f40f308e3e60ea11ca
artyomzavalin <artyomzavalin@gmail.com>
2024-01-07 14:41:44 -0500

    New Class UrlTest added

---
+++ src/HomePageAccess/UrlTest.java
@@ -0,0 +1,41 @@
+package HomePageAccess;
+import Utilities.MyMethods;
+import org.junit.Assert;
+import org.junit.Test;
+import Utilities.DriverClass;
+import com.google.common.annotations.VisibleForTesting;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class UrlTest extends DriverClass {
+
+    @Test
+
+    public void accessToHomepage() {
+
+        driver.get("https://www.lidl.com/");
+
+
+        WebElement closeGreenTab = driver.findElement(By.cssSelector("img[alt=\"close-white description\"]"));
+        closeGreenTab.click();
+
+        WebElement banner = driver.findElement(By.cssSelector("div[id=\"onetrust-banner-sdk\"]"));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOf(banner));
+
+        WebElement acceptCookies = driver.findElement(By.cssSelector("div[id=\"onetrust-close-btn-container\"]"));
+        acceptCookies.click();
+
+        String expectedURL = "https://www.lidl.com/";
+
+        Assert.assertTrue(driver.getCurrentUrl().equals(expectedURL));
+    }
+}
+
+   
