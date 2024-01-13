7d3b35da395f64c1b36c45e86edf86d393495201
shaym <shaymaa2012@gmail.com>
2024-01-12 23:01:42 -0500

    This is my new version of the code,

--- src/Login_Functionality/Register_Test.java
+++ src/Login_Functionality/Register_Test.java
@@ -4,7 +4,6 @@ import Utilities.DriverClass;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
