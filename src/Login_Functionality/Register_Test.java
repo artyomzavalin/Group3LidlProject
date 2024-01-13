da686709c64971096256b0bfdefd88b97f96baa7
shaym <shaymaa2012@gmail.com>
2024-01-12 22:33:36 -0500

    This is my new version of the code,

--- src/Login_Functionality/Register_Test.java
+++ src/Login_Functionality/Register_Test.java
@@ -16,49 +16,50 @@ public class Register_Test extends DriverClass {
     @Test
   public void registerTest(){
 
-  driver.get("https://www.lidl.com/");
+        driver.get("https://www.lidl.com/");
 
-
-    WebElement closeGreenTab = driver.findElement(By.cssSelector("img[alt=\"close-white description\"]"));
+        WebElement closeGreenTab = driver.findElement(By.cssSelector("img[alt=\"close-white description\"]"));
         closeGreenTab.click();
 
-    WebElement banner = driver.findElement(By.cssSelector("div[id=\"onetrust-banner-sdk\"]"));
+        WebElement banner = driver.findElement(By.cssSelector("div[id=\"onetrust-banner-sdk\"]"));
 
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
         wait.until(ExpectedConditions.visibilityOf(banner));
 
-    WebElement acceptCookies = driver.findElement(By.cssSelector("div[id=\"onetrust-close-btn-container\"]"));
+        WebElement acceptCookies = driver.findElement(By.cssSelector("div[id=\"onetrust-close-btn-container\"]"));
         acceptCookies.click();
 
-    String expectedURL = "https://www.lidl.com/";
+        WebElement signIn = driver.findElement(By.linkText("sign in"));
+        signIn.click();
 
-        Assert.assertTrue(driver.getCurrentUrl().equals(expectedURL));
-
-    JavascriptExecutor js = (JavascriptExecutor) driver;
-
-
-    WebElement joinMyLidl = driver.findElement(By.cssSelector("a[data-test=\"register\"]"));
-
-
-        js.executeScript("arguments[0].scrollIntoView(true);", joinMyLidl);
-
+        WebElement joinMyLidl = driver.findElement(By.cssSelector("a[data-test=\"joinButton\"]"));
         joinMyLidl.click();
 
-    WebElement firstName = driver.findElement(By.cssSelector("input[id=\"input0\"]"));
+
+        String expectedURL = "https://www.lidl.com/register";
+        Assert.assertTrue(driver.getCurrentUrl().equals(expectedURL));
+
+
+        WebElement firstName = driver.findElement(By.cssSelector("input[name=\"firstName\"]"));
         firstName.sendKeys("Leeno");
 
 
-    WebElement lastName = driver.findElement(By.cssSelector("input[name=\"lastName\"]"));
+        WebElement lastName = driver.findElement(By.cssSelector("input[name=\"lastName\"]"));
         lastName.sendKeys("Adam");
 
 
-    WebElement email = driver.findElement(By.cssSelector("input[name=\"email\"]"));
-        email.sendKeys("leeno2020@gmail.com");
+        WebElement emailInput = driver.findElement(By.cssSelector("input[name=\"email\"]"));
+        emailInput.sendKeys("leeno2020@gmail.com");
 
+        WebElement errorMessage = driver.findElement(By.cssSelector("span[class=\"email-taken-inline-error\"]"));
 
-    WebElement confirmEmail = driver.findElement(By.cssSelector("input[name=\"confirm_email\"]"));
-        confirmEmail.sendKeys("leeno2020@gmail.com");
+        if (errorMessage.getText().contains("This email is already registered. Want to sign in or recover your password?")) {
+            System.out.println("Test Passed: Correct error message displayed.");
+
+        } else {
+            System.out.println("Test Failed: Incorrect error message displayed.");
+        }
+
+    }
 
 }
-}
-
